72ade52a49730d12a881f91c4869658b3ff1e198
sondrb <sondrb@netcompany.com>
2018-11-08 16:47:59 +0100

    changed localhost dir

--- src/Auth/AuthConfig.js
+++ src/Auth/AuthConfig.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'berdal.eu.auth0.com',
   clientId: 'poY5izER2q9NxjunRA7WtfzUzgtZzhGf',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://berdal.herokuapp.com/callback'
 };
